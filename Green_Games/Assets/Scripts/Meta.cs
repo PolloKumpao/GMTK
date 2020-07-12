a70740707d57f0809d476e3328a3c67a4bfd8b00
Keder <edufs96@gmail.com>
2020-07-12 15:14:01 +0200

    Todo Implementado

--- Green_Games/Assets/Scripts/Meta.cs
+++ Green_Games/Assets/Scripts/Meta.cs
@@ -94,8 +94,8 @@ public class Meta : MonoBehaviour
             {
                 //myRigidbody2D.velocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
                 //Debug.Log("Colision");
-                Objetivo--;
-                collision.gameObject.SetActive(false);
+                //Objetivo--;
+                //collision.gameObject.SetActive(false);
 
 
             }
