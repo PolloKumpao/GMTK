e91eb847e8de894e9737c0b9b638e7e3cea0e5a2
Adri <8.mrlord.8@gmail.com>
2020-07-12 18:55:59 +0200

    7niveles

--- Green_Games/Assets/Scripts/Meta.cs
+++ Green_Games/Assets/Scripts/Meta.cs
@@ -54,9 +54,13 @@ public class Meta : MonoBehaviour
             {
                 //myRigidbody2D.velocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
                 //Debug.Log("Colision");
-                //Objetivo++;
-                //collision.gameObject.SetActive(false);
-                //FX.Play();
+                if (Objetivo > 0)
+                {
+                    Objetivo++;
+                    collision.gameObject.SetActive(false);
+                    FX.Play();
+                }
+               
 
             }
             else if (collision.gameObject.tag == "Player")
@@ -75,13 +79,14 @@ public class Meta : MonoBehaviour
             {
                 //myRigidbody2D.velocity = collision.gameObject.GetComponent<Rigidbody2D>().velocity;
                 //Debug.Log("Colision");
-                //if (Objetivo > 0)
-                //{
-                //    Objetivo++;
-                //}
+                if (Objetivo > 0)
+                {
+                    Objetivo++;
+                    collision.gameObject.SetActive(false);
+                    FX.Play();
+                }
 
-                //collision.gameObject.SetActive(false);
-                //FX.Play();
+              
 
             }
             else if (collision.gameObject.tag == "-")
